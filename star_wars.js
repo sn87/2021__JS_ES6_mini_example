c3dbefd87e23dbe3f62a4581b9fbcca78dbfa4ac
sn87 <sonone@web.de>
2021-08-20 20:11:39 +0200

    Update star_wars.js

--- star_wars.js
+++ star_wars.js
@@ -1,5 +1,5 @@
 const protagonists = [{
-    name: 'Luke Skywalker',
+    name: 'Luke',
     team: 'rebels',
     karmaScore: 2
   },
@@ -20,6 +20,8 @@ const protagonists = [{
   }
 ]
 
+let winningTeam = 'rebels';
+
 console.log(protagonists);
 
 const rebels = protagonists.filter(function(protagonist) {
@@ -30,11 +32,35 @@ const tenTimesMoreEvilEmpire = [];
 protagonists.map(function(protagonist) {
   if(protagonist.team === 'empire'){
   	protagonist.karmaScore = protagonist.karmaScore*10;
-    tenTimesMoreEvilEmpire.push(protagonist);
+    tenTimesMoreEvilEmpire.push(protagonist);    
   }
 });
 
 console.log(rebels);
 console.log(tenTimesMoreEvilEmpire);
 
-/* The Saga continues */
+// positive value: empire wins, negative value: rebels win
+let battleValue = battle(rebels, tenTimesMoreEvilEmpire);
+console.log(winningTeam)
+if (battleValue > 0) {
+	winningTeam = 'rebels';
+}else if (battleValue < 0) {
+	winningTeam = 'empire';
+}
+console.log(winningTeam)
+
+// function to determine winning team
+function battle(team1, team2) {
+  winningTeam = 'draw';
+  let team1Power = 0;
+  let team2Power = 0;
+  team1.forEach(function(teamMate) {
+      team1Power += teamMate.karmaScore;
+  })
+  team2.forEach(function(teamMate) {
+      team2Power += teamMate.karmaScore;
+  })
+  return team1Power + team2Power;
+}
+
+/* --- The Saga continues --- */
