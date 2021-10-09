239116f0ccd6219e4711e47dc9fe20e115bae2ea
sn87 <sonone@web.de>
2021-10-09 15:16:45 +0200

    Update star_wars.js

--- star_wars.js
+++ star_wars.js
@@ -11,43 +11,49 @@ const protagonists = [{
   {
     name: 'Yoda',
     team: 'rebels',
-    karmaScore: 4
+    karmaScore: 4,
+    specialPower: copyForce
   },
   {
     name: 'Emperor Palpatine',
     team: 'empire',
-    karmaScore: -4
+    karmaScore: -4,
+    specialPower: copyForce
   }
 ]
 
-let winningTeam = 'rebels';
+let winningTeam = 'draw';
 
-console.log(protagonists);
+const rebels = protagonists.filter(protagonist => protagonist.team === 'rebels');
 
-const rebels = protagonists.filter(function(protagonist) {
-  return protagonist.team === 'rebels';
-});
-
+// dramatic increase of empires force due to a flyby of the Death Star at a supermassive black hole, where the tenTimesMoreEvilEmpire from a parallel universe replaced the normalEmpire 
 const tenTimesMoreEvilEmpire = [];
-protagonists.map(function(protagonist) {
+protagonists.map(protagonist => {
   if(protagonist.team === 'empire'){
   	protagonist.karmaScore = protagonist.karmaScore*10;
     tenTimesMoreEvilEmpire.push(protagonist);    
   }
 });
 
+const yoda = protagonists.find(protagonist => protagonist.name === 'Yoda');
+
 console.log(rebels);
 console.log(tenTimesMoreEvilEmpire);
 
+console.log(yoda);
+// use of special power by yoda to save the universe
+yoda.specialPower('empire', -1);
+console.log(yoda);
+
 // positive value: rebels wins, negative value: empire wins
 let battleValue = battle(rebels, tenTimesMoreEvilEmpire);
-console.log(winningTeam)
 if (battleValue > 0) {
 	winningTeam = 'rebels';
 }else if (battleValue < 0) {
 	winningTeam = 'empire';
 }
-console.log(winningTeam)
+
+console.log(`Winning Team: ${winningTeam}`);
 
 // function to determine winning team
 function battle(team1, team2) {
@@ -63,4 +69,16 @@ function battle(team1, team2) {
   return team1Power + team2Power;
 }
 
-/* --- The Saga continues --- */
+// function to copy all force of the enemy team onto one protagonist
+// with the inital value being the protagonists current karmaScore
+function copyForce(enemyTeam, multiplier) { 	
+  this.karmaScore =  protagonists.reduce((prev, protagonist) => {
+  	if (protagonist.team == enemyTeam) {
+      let tempVal = protagonist.karmaScore;
+      if (protagonist.karmaScore  < 0) {tempVal = tempVal * multiplier;} 
+      return prev + tempVal
+    }else{
+    	return prev;
+    }
+  }, this.karmaScore)
+}
